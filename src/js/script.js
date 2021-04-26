1af39394f6940a22ff7bf590faf1df1f9f45a2e0
hanii-gerges <hanigerges05@gmail.com>
2021-04-27 00:12:08 +0300

    build: apply/contact/footer sections

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,14 @@ $(window).on('scroll',function(){
     if($(window).scrollTop()>70){
         $('nav').addClass('scroll');
         $('nav .navbar-brand').removeClass('d-md-none');
-        $('nav .navbar-brand').addClass('mx-5');
+        $('nav .navbar-brand').addClass('mx-md-5');
         $('nav ul').addClass('ml-5');
 
     }
     else{
         $('nav').removeClass('scroll');
         $('nav .navbar-brand').addClass('d-md-none');
-        $('nav .navbar-brand').removeClass('mx-5');
+        $('nav .navbar-brand').removeClass('mx-md-5');
         $('nav ul').removeClass('ml-5');
     }
     //console.log($(window).scrollTop())
