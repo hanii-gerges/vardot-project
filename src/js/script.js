63558527c64dab6668e22f222ecd96f3b01d2131
hanii-gerges <hanigerges05@gmail.com>
2021-04-28 04:06:21 +0300

    fix: responsive font size and ui modified

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,30 @@
 $(window).on('scroll',function(){
     if($(window).scrollTop()>70){
         $('nav').addClass('scroll');
-        $('nav .navbar-brand').removeClass('d-md-none');
-        $('nav .navbar-brand').addClass('mx-md-5');
-        $('nav ul').addClass('ml-5');
+        $('nav .navbar-brand').addClass('d-xl-block');
+        $('.collapse').addClass('justify-content-xl-end');
+        $('.navbar-nav li').addClass('mx-xl-2');
 
     }
     else{
         $('nav').removeClass('scroll');
-        $('nav .navbar-brand').addClass('d-md-none');
-        $('nav .navbar-brand').removeClass('mx-md-5');
-        $('nav ul').removeClass('ml-5');
+        $('nav .navbar-brand').removeClass('d-xl-block');
+        $('.collapse').removeClass('justify-content-xl-end');
+        $('.navbar-nav li').removeClass('mx-xl-2');
     }
     //console.log($(window).scrollTop())
 })
+
+$(function () {
+    $('a[href="#search"]').on('click', function(event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    
+    $('#search, #search button.close').on('click keyup', function(event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
+});
