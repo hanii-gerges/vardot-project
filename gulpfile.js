c3b1654ab2cf654b6b99b0f5598e39d0d1b92636
hanii-gerges <hanigerges05@gmail.com>
2021-05-24 22:33:15 +0300

    change gulp root

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,32 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 //compile scss into css
 function frontendStyle() {
-    return gulp.src('public_html/scss/**/*.scss')
+    return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('public_html/css'))
+    .pipe(gulp.dest('src/css'))
     .pipe(browserSync.stream());
 }
 
 function backendStyle() {
-    return gulp.src('public_html/admin/assets/scss/**/*.scss')
+    return gulp.src('src/admin/assets/scss/**/*.scss')
     .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('public_html/admin/assets/css'))
+    .pipe(gulp.dest('src/admin/assets/css'))
     .pipe(browserSync.stream());
 }
 
 function watch() {
     browserSync.init({
         server: {
-           baseDir: "./public_html",
+           baseDir: "./src",
            index: "/index.html"
         }
     });
-    gulp.watch('public_html/scss/**/*.scss', frontendStyle)
-    gulp.watch('public_html/*.html').on('change',browserSync.reload)
-    gulp.watch('public_html/js/**/*.js').on('change', browserSync.reload)
-    gulp.watch('public_html/admin/**/*.html').on('change',browserSync.reload)
-    gulp.watch('public_html/admin/assets/**/*.scss', backendStyle)
-    gulp.watch('public_html/admin/assets/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('src/scss/**/*.scss', frontendStyle)
+    gulp.watch('src/*.html').on('change',browserSync.reload)
+    gulp.watch('src/js/**/*.js').on('change', browserSync.reload)
+    gulp.watch('src/admin/**/*.html').on('change',browserSync.reload)
+    gulp.watch('src/admin/assets/**/*.scss', backendStyle)
+    gulp.watch('src/admin/assets/js/**/*.js').on('change', browserSync.reload);
 
 }
 exports.frontendStyle = frontendStyle;
